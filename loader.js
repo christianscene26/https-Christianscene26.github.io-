7a26a57882d1765d624a0b331777fa75540bdbe0
christianscene26 <106438528+christianscene26@users.noreply.github.com>
2022-07-01 16:40:34 +0100

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,27 @@
+ var Name = prompt("what whether location are you asking for?");
+if(Name="SanFrancisco"){
+     console.log(
+         ["location: North California","Temperature: 37^F"]
+     )
+ }
+else{
+    prompt("Weather does not exist try Again!");
+}
+
+// NEXT PageTransitionEvent
+
+// var index = 0;
+//change();
+
+//function change() {
+    //var x = document.getElementsByClassName('slides');
+    //for(var i = 0;i < x.length; i++){
+       // x[i].style.display = "none";
+   // }
+   // index++;
+   // if(index > x.length){
+       // index= 1;
+   // }
+   // x[index -1].style.display = "block";
+   // setTimeout(change, 3000);
+//}
